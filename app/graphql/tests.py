e7d3f367da437ca7acffbb7ace994c1865358291
Artsem Stalavitski <a.stalavitski@gmail.com>
2017-12-25 10:23:04 +0300

    added test operations constructor

--- app/graphql/tests.py
+++ app/graphql/tests.py
@@ -8,6 +8,63 @@ from django.http import HttpRequest
 from django.test import TestCase
 
 
+class Operation:
+    type = None
+
+    def __init__(self, name, output: dict, input: dict = None):
+        self.name = name
+        self.output = output
+        self.input = input
+
+    def get_output_result(self, output, spaces: int = 2) -> str:
+        result = ' { \n'
+        spaces_string = ' ' * spaces
+
+        for field, value in output.items():
+            if type(value) is dict:
+                result += spaces_string + '  ' + field + self.get_output_result(value, spaces + 2)
+            else:
+                result += spaces_string + '  ' + field + '\n'
+
+        result += spaces_string + '} \n'
+        return result
+
+    def get_input_result(self) -> str:
+        result = ''
+
+        if self.input is not None:
+            result += '(input: { \n'
+
+            for field, value in self.input.items():
+                if type(value) is str:
+                    value = '"' + value + '"'
+
+                result += '    ' + field + ': ' + str(value) + '\n'
+
+            result += '  })'
+
+        return result
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_result(self) -> str:
+        result = self.type + ' ' + self.name + ' { \n'
+        result += '  ' + self.name
+        result += self.get_input_result()
+        result += self.get_output_result(self.output)
+        result += '}'
+        return result
+
+
+class Query(Operation):
+    type = 'query'
+
+
+class Mutation(Operation):
+    type = 'mutation'
+
+
 class GraphqlTestCase(TestCase):
     def assert_raised_error(self, result: dict, error_message: str):
         self.assert_operation_errors(result)
--- app/user/tests.py
+++ app/user/tests.py
@@ -1,4 +1,4 @@
-from app.graphql.tests import GraphqlTestCase
+from app.graphql.tests import GraphqlTestCase, Mutation, Query
 from app.graphql.api import schema
 from django.contrib.auth.models import User
 from graphene.test import Client
@@ -8,23 +8,36 @@ class SchemaTestCase(GraphqlTestCase):
     def setUp(self):
         super(SchemaTestCase, self).setUp()
 
-    def get_create_user_mutation(self) -> str:
-        return '''
-            mutation createUser { 
-                createUser(input: {
-                    email: "''' + self.get_user_email('get_create_user_mutation') + '''",
-                    password: "get_create_user_mutation"
-                }) {
-                    ok
+    def get_create_user_mutation(self) -> Mutation:
+        return Mutation('createUser', {'ok': ''}, {
+            'email': self.get_user_email('get_create_user_mutation'),
+            'password': 'get_create_user_mutation'
+        })
+
+    def get_current_user_query(self) -> Query:
+        return Query('currentUser', {
+            'companyMemberships': {
+                'edges': {
+                    'node': {
+                        'pk': '',
+                        'points': '',
+                        'availableRewards': {
+                            'edges': {
+                                'node': {
+                                    'pk': ''
+                                }
+                            }
+                        }
+                    }
                 }
             }
-            '''
+        })
 
     # test createUser migration
     def test_create_user(self):
         count = User.objects.count()
+        mutation = self.get_create_user_mutation()
         result = Client(schema).execute(
-            self.get_create_user_mutation(), context_value=self.get_context_value())
-        print(result)
-        self.assert_mutation_success(result, 'createUser')
+            mutation.get_result(), context_value=self.get_context_value())
+        self.assert_mutation_success(result, mutation.get_name())
         self.assertEqual(User.objects.count(), count + 1, 'Check if user has been created')
