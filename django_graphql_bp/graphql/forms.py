5fc9fd18d36a2b98ecfd0d089300e697fd5b71ca
Artsem Stalavitski <a.stalavitski@gmail.com>
2018-01-20 15:18:29 +0000

    added UpdateForm + updated UpdateUserForm

---
+++ django_graphql_bp/graphql/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.forms.fields import FileField
+
+
+class UpdateForm(forms.ModelForm):
+    def _clean_fields(self):
+        for name, field in self.fields.items():
+            if field.disabled:
+                value = self.get_initial_for_field(field, name)
+            else:
+                value = field.widget.value_from_datadict(self.data, self.files, self.add_prefix(name))
+
+                if not value:
+                    value = getattr(self.instance, name)
+
+            try:
+                if isinstance(field, FileField):
+                    initial = self.get_initial_for_field(field, name)
+                    value = field.clean(value, initial)
+                else:
+                    value = field.clean(value)
+
+                self.cleaned_data[name] = value
+
+                if hasattr(self, 'clean_%s' % name):
+                    value = getattr(self, 'clean_%s' % name)()
+                    self.cleaned_data[name] = value
+            except ValidationError as e:
+                self.add_error(name, e)
--- django_graphql_bp/user/forms.py
+++ django_graphql_bp/user/forms.py
@@ -1,3 +1,4 @@
+from django_graphql_bp.graphql.forms import UpdateForm
 from django_graphql_bp.user.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
@@ -14,7 +15,7 @@ class CreateUserForm(UserCreationForm):
         return email
 
 
-class UpdateUserForm(forms.ModelForm):
+class UpdateUserForm(UpdateForm):
     email = forms.EmailField(required=False)
     is_active = forms.BooleanField(required=False)
     name = forms.CharField(required=False)
@@ -22,14 +23,5 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'is_active', 'name']
-
-    def clean(self) -> dict:
-        cleaned_data = super(UpdateUserForm, self).clean()
-
-        for field in self.fields:
-            if field not in self.data.keys():
-                cleaned_data[field] = getattr(self.instance, field)
-
-        return cleaned_data
 
 
